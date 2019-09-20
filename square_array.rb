8cfd284d2c2980727caf31aaebdb2fb19d164f72
John Reyes <john.jreye@gmail.com>
2019-09-20 15:18:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array
 def square!
   numbers = [1,2,3]
   numbers.each.do |square|
-    puts "This number is now #{square}"
+    puts "This number is now #{square}."
   end
   end
 [1,2,3].square!
